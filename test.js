fbd5c44fb0b7f72663c36bca9e04f3c0f574fa82
Kresimir Ivkovic <kivkovic@gmail.com>
2018-02-18 14:52:16 +0100

    Expanded test

--- test.js
+++ test.js
@@ -10,13 +10,55 @@ class A {
         this.g = new RegExp('ab.*c', 'gi');
         this.h = [1,2,3,new Date(), {a: Symbol('abc')}, "dddddddd"];
         this.i = new WeakMap();
-        // TODO:
-        // does prop order matter?
-        // reduce number of calls in hash, maybe immediatelly stringify if primitive
-        // arraybuffer
-        // setiterator
-        // mapiterator
-        // general iterator...?
+        this.j = {  
+           a:{  
+              a:{  
+                 a:{  
+                    a:{a:1,b:2,c:3,d:4},
+                    b:[  
+                       {  
+                          a:{a:1,b:2,c:3,d:4},
+                          b:[  
+                             { a:{a:1,b:2,c:3,d:4}, b:[1,2,3], c:1 },
+                             { a:{a:1,b:2,c:3,d:4}, b:[4,5,6], c:1 },
+                             { a:{a:1,b:2,c:3,d:4}, b:[7,8,9], c:1 },
+                             { a:{a:1,b:2,c:3}, b:[10,11,12], c:1 },
+                             { a:{a:1,b:2}, b:[13,14,15,16,17,18], c:1 }
+                          ],
+                          c:1
+                       },
+                       {  
+                          a:{a:1,b:2,c:3,d:4},
+                          b:[ { a:{a:1,b:2,c:3,d:4}, b:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], c:1 } ],
+                          c:1
+                       }
+                    ],
+                    c:1
+                 },
+                 b:[  
+                    { a:{a:1,b:2,c:3,d:4}, b:[1,2,3,4,5,6,7,8,9,10,11,12,13], c:1 },
+                    { a:{}, b:[1,2,3,4,5,6,7,8,9,10,11,12,13,14], c:1 }
+                 ],
+                 c:1
+              },
+              b:[  
+                 {  
+                    a:{},
+                    b:[  
+                       { a:{a:1,b:2,c:3,d:4}, b:[2,3,4,5,6,7,8,9,10,11,12,13,14,15], c:1 },
+                       { a:{}, b:[3,4,5,6,7,8,9,10,11,12,13,14,15], c:1 }
+                    ],
+                    c:1
+                 },
+                 { a:{a:1,b:2,c:3,d:4}, b:[4,5,6,7,8,9,10,11,12,13,14,15], c:1 },
+                 { a:{}, b:[5,6,7,8,9,10,11,12,13,14,15], c:1 },
+                 { a:{a:1,b:2,c:3,d:4}, b:[6,7,8,9,10,11,12,13,14,15], c:1 }
+              ],
+              c:1
+           },
+           b:[1,2,3,4,5,6,7,8,9,10],
+           c:1
+        };
     }
 
     c() {
@@ -25,23 +67,27 @@ class A {
 }
 
 const list = [];
-for (let i = 0; i < 100000; i++) { // 12s
-    list.push(new A(i));
+for (let i = 0; i < 100000; i++) {
+    list.push(new A(i + Math.random()));
 }
 
+let results = [];
+let start = +new Date;
 
-const start = +new Date;
 for (let i = 0; i < list.length; i++) {
-    serialize(list[i]);
+    results.push(serialize(list[i]));
 }
-console.log('Time:', +new Date - start);
+
+console.log('jserialize time:', +new Date - start);
+
+results = [];
+start = +new Date;
+
+for (let i = 0; i < list.length; i++) {
+    results.push(JSON.stringify(list[i]));
+}
+
+console.log('JSON time:', +new Date - start);
 
 const first = new A(0);
-//const second = new A(0);
-//first.a = 1;
-//first.b = 2;
-//second.b = 1;
-//second.b = 2;
-
 console.log(serialize(first));
-//console.log(hash(second));
