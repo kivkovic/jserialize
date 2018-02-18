369dfa0e63055a17d3c642374caffbdffc868de1
Kresimir Ivkovic <kivkovic@gmail.com>
2018-02-18 14:30:00 +0100

    Rearranged serailize function

--- index.js
+++ index.js
@@ -2,56 +2,51 @@ exports.default = function serialize(value) {
 
     if (value === null) return 'null';
     if (value === undefined) return 'undefined';
+    if (typeof value === 'number') return value;
+    if (typeof value === 'string') return `"${value.replace('"','\\"')}"`;
 
-    switch (typeof value) {
-        case 'object':
-        case 'function':
-        case 'symbol':
+    if (typeof value !== 'object'
+        && typeof value !== 'function'
+        && typeof value !== 'symbol') {
+        return '\0';
+    }
 
-        switch (value.constructor) {
+    switch (value.constructor) {
 
-            case WeakMap:
-            case WeakSet:
-                return `${value.constructor.name}{}`;
+        case WeakMap:
+        case WeakSet:
+            return `${value.constructor.name}{}`;
 
-            case Symbol:
-                return value.toString();
+        case Symbol:
+            return value.toString();
 
-            case Function:
-            case RegExp:
-                return `${value.constructor.name}{${value.toString()}}`;
+        case Function:
+        case RegExp:
+            return `${value.constructor.name}{${value.toString()}}`;
 
-            case Date:
-                return `Date(${Number(value)})`;
+        case Date:
+            return `Date(${Number(value)})`;
 
-            case Array:
-                return `[${value.map(entry => serialize(entry)).join(',')}]`;
+        case Array:
+            return `[${value.map(entry => serialize(entry)).join(',')}]`;
 
-            case Set:
-                var inner = [];
-                value.forEach(entry => { inner.push(serialize(entry)); });
-                return `Set{${inner.join(',')}}`;
+        case Set:
+            var inner = [];
+            value.forEach(entry => { inner.push(serialize(entry)); });
+            return `Set{${inner.join(',')}}`;
 
-            case Map:
-                var inner = [];
-                value.forEach((entry, key) => { inner.push(`${serialize(key)}:${serialize(entry)}`); });
-                return `Map{${inner.join(',')}}`;
+        case Map:
+            var inner = [];
+            value.forEach((entry, key) => { inner.push(`${serialize(key)}:${serialize(entry)}`); });
+            return `Map{${inner.join(',')}}`;
 
-            default:
-                return `${value.constructor !== Object ? 
-                    `class ${value.constructor.name}` : ''}{${
-                        Object.keys(value)
-                        .map(key => `${serialize(key)}:${serialize(value[key])}`)
-                        .join(',')}}`;
-            }
-
-        case 'number':
-            return value;
-        
-        case 'string':
-            return `"${value.replace('"','\\"')}"`;
-        
         default:
-            return '\0';
+            return `${value.constructor !== Object
+                ? `class ${value.constructor.name}`
+                : ''}{${
+                    Object.keys(value)
+                        .map(key => `${serialize(key)}:${serialize(value[key])}`)
+                        .join(',')
+                    }}`;
     }
 }
