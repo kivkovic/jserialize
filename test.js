80f81c88e3b0645a25f5ee9752c0ffb96e28da5a
Kresimir Ivkovic <kivkovic@gmail.com>
2018-02-18 17:03:49 +0100

    Randomized test entries, reordered

--- test.js
+++ test.js
@@ -10,53 +10,53 @@ class A {
         this.g = new RegExp('ab.*c', 'gi');
         this.h = [1,2,3,new Date(), {a: Symbol('abc')}, "dddddddd"];
         this.i = new WeakMap();
-        this.j = {  
-           a:{  
-              a:{  
-                 a:{  
-                    a:{a:1,b:2,c:3,d:4},
-                    b:[  
-                       {  
-                          a:{a:1,b:2,c:3,d:4},
-                          b:[  
-                             { a:{a:1,b:2,c:3,d:4}, b:[1,2,3], c:1 },
-                             { a:{a:1,b:2,c:3,d:4}, b:[4,5,6], c:1 },
-                             { a:{a:1,b:2,c:3,d:4}, b:[7,8,9], c:1 },
-                             { a:{a:1,b:2,c:3}, b:[10,11,12], c:1 },
-                             { a:{a:1,b:2}, b:[13,14,15,16,17,18], c:1 }
+        this.j = {
+           a:{
+              a:{
+                 a:{
+                    a:{a:Math.random(),b:2,c:3,d:4},
+                    b:[
+                       {
+                          a:{a:1,b:Math.random(),c:3,d:4},
+                          b:[
+                             { a:{a:1,b:2,c:Math.random(),d:4}, b:[1,2,3], c:1 },
+                             { a:{a:1,b:2,c:3,d:4}, b:[4,Math.random(),6], c:1 },
+                             { a:{a:1,b:Math.random(),c:3,d:4}, b:[7,8,9], c:Math.random() },
+                             { a:{a:1,b:2,c:3}, b:[10,11,Math.random()], c:1 },
+                             { a:{a:1,b:2}, b:[Math.random(),14,15,16,17,18], c:1 }
                           ],
                           c:1
                        },
-                       {  
-                          a:{a:1,b:2,c:3,d:4},
-                          b:[ { a:{a:1,b:2,c:3,d:4}, b:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], c:1 } ],
+                       {
+                          a:{a:1,b:Math.random(),c:3,d:4},
+                          b:[ { a:{a:1,b:2,c:3,d:Math.random()}, b:[1,2,3,4,Math.random(),6,7,8,9,10,11,12,13,14,15], c:Math.random() } ],
                           c:1
                        }
                     ],
                     c:1
                  },
-                 b:[  
-                    { a:{a:1,b:2,c:3,d:4}, b:[1,2,3,4,5,6,7,8,9,10,11,12,13], c:1 },
-                    { a:{}, b:[1,2,3,4,5,6,7,8,9,10,11,12,13,14], c:1 }
+                 b:[
+                    { a:{a:1,b:Math.random(),c:3,d:4}, b:[1,2,3,4,5,6,Math.random(),8,9,10,11,12,13], c:1 },
+                    { a:{}, b:[1,2,3,4,5,6,Math.random(),8,9,10,11,12,13,Math.random()], c:1 }
                  ],
                  c:1
               },
-              b:[  
-                 {  
+              b:[
+                 {
                     a:{},
-                    b:[  
-                       { a:{a:1,b:2,c:3,d:4}, b:[2,3,4,5,6,7,8,9,10,11,12,13,14,15], c:1 },
-                       { a:{}, b:[3,4,5,6,7,8,9,10,11,12,13,14,15], c:1 }
+                    b:[
+                       { a:{a:Math.random(),b:2,c:3,d:4}, b:[2,3,4,5,6,7,8,9,10,11,Math.random(),13,14,15], c:1 },
+                       { a:{}, b:[3,4,5,6,Math.random(),8,9,10,11,12,13,14,15], c:1 }
                     ],
                     c:1
                  },
-                 { a:{a:1,b:2,c:3,d:4}, b:[4,5,6,7,8,9,10,11,12,13,14,15], c:1 },
-                 { a:{}, b:[5,6,7,8,9,10,11,12,13,14,15], c:1 },
-                 { a:{a:1,b:2,c:3,d:4}, b:[6,7,8,9,10,11,12,13,14,15], c:1 }
+                 { a:{a:1,b:2,c:3,d:Math.random()}, b:[Math.random(),4,5,6,7,8,9,10,11,12,13,14,15], c:1 },
+                 { a:{}, b:[5,6,7,8,9,10,11,12,13,14,15], c:Math.random() },
+                 { a:{a:1,b:2,c:3,d:4}, b:[6,7,Math.random(),9,10,11,12,13,14,15], c:1 }
               ],
               c:1
            },
-           b:[1,2,3,4,5,6,7,8,9,10],
+           b:[1,2,3,4,5,6,7,8,9,10,Math.random()],
            c:1
         };
     }
@@ -67,12 +67,24 @@ class A {
 }
 
 const list = [];
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 20000; i++) {
     list.push(new A(i + Math.random()));
 }
 
-let results = [];
-let start = +new Date;
+let results;
+let start;
+
+start = +new Date;
+results = [];
+
+for (let i = 0; i < list.length; i++) {
+    results.push(JSON.stringify(list[i]));
+}
+
+console.log('JSON time:', +new Date - start);
+start = +new Date;
+
+results = [];
 
 for (let i = 0; i < list.length; i++) {
     results.push(serialize(list[i]));
@@ -80,14 +92,6 @@ for (let i = 0; i < list.length; i++) {
 
 console.log('jserialize time:', +new Date - start);
 
-results = [];
-start = +new Date;
-
-for (let i = 0; i < list.length; i++) {
-    results.push(JSON.stringify(list[i]));
-}
-
-console.log('JSON time:', +new Date - start);
 
 const first = new A(0);
 console.log(serialize(first));
